923dd149b055c182bf7b139f7f77d5829f8e4b03
Iryna Rudenko <irinka.rude@gmail.com>
2023-06-06 01:42:42 +0200

    promises-form

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onSubmit);
+
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const { elements: { delay, step, amount } } = event.currentTarget;
+
+  let delayInput = Number(delay.value);
+  const stepInput = Number(step.value);
+  const amountInput = Number(amount.value);
+
+  for (let i = 1; i <= amountInput; i += 1) {
+    createPromise(i, delayInput)
+      .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    
+    delayInput = delayInput + stepInput;
+  };
+
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+    const shouldResolve = Math.random() > 0.3;
+
+    if (shouldResolve) {
+      resolve({ position, delay })
+      }
+    reject({ position, delay })
+    }, delay)
+  });
+};
